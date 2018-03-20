8cf1a4a9fdbb8d3e74e2c0375b0f39a0b7aa87d8
Jasneek chugh <chughjasneek@gmail.com>
2018-03-21 01:15:48 +0530

    Program to print n copies of a given string
    INPUT-
    Hello
    3
    
    OUTPUT-
    HelloHelloHello

---
+++ repeat_String_n_times.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class repeat_String_n_times
+{
+     public Static void main(String args[])
+       String result= repeatStringntimes("Hello",3);
+    System.out.print(result);
+
+public Static string repeatStringntimes( string str, int n)
+ {
+  String str="";
+  while(n!=0)
+  {
+      result +=str;
+      n--;
+  }
+  return result;
+
+ }
+
+}
